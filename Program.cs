6a5d42363b085aa2eddba5dc28b176b6cddab9b8
Elliotsdreed <elliotsdreed@icloud.com>
2019-10-03 13:21:26 +0100

    near finish

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace homework_week_1_part_2
     {
         static void Main(string[] args)
         {
-             
+         rectangle firstrectangle = new rectangle(2,4);
+         Console.WriteLine("The Area is{0}",firstrectangle.getarea());    
         }
     }
 }
